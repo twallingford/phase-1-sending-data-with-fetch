3d375fef41bfe4405194ccbb0bc5bca355c43bce
Thomas Wallingford <tbwallingford@gmail.com>
2023-03-19 10:33:44 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(userName, userEmail){
+    const b = document.body 
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": userName, 
+            "email": userEmail})
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        console.log(object.id)
+        b.innerHTML = object.id;
+    })
+    .catch(function (error) {
+        console.log(error.message)
+        b.innerHTML = error.message;
+    })
+}
